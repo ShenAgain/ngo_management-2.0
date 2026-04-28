ad20c585ae945e7e5a093be9eb381200bb029c4d
ShennyBoi <B2201451@helplive.edu.my>
2026-04-28 14:03:31 +0800

    fix swagger api

--- ngo_management/urls.py
+++ ngo_management/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework.permissions import AllowAny
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,8 +13,8 @@ urlpatterns = [
     # Topic 8: RESTful APIs
     path('api/v1/', include('ngo.urls_api')),
     # Swagger / OpenAPI
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/', SpectacularAPIView.as_view(permission_classes=[AllowAny]), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema', permission_classes=[AllowAny]), name='swagger-ui'),
     # Redirect root to activity list
     path('', lambda request: redirect('ngo:activity_list')),
 ]
